fed26f11154749557fbef94b5399ec98c0f6f57e
Lain <2836969767@qq.com>
2026-05-08 17:30:18 +0800

    auto: blog post update 2026-05-08 09:30:18

--- scripts/sync-from-obsidian.sh
+++ scripts/sync-from-obsidian.sh
@@ -3,7 +3,7 @@
 # Only syncs .md/.mdx files from the "blogs" folder
 
 VAULT_DIR="$HOME/Documents/Obsidian Vault/blogs"
-BLOG_DIR="$HOME/Documents/personal-website/content/posts"
+BLOG_DIR="$HOME/Documents/blog/content/posts"
 
 # Use rsync to sync (with --delete, Obsidian Vault is the source of truth)
 rsync -av --delete \
